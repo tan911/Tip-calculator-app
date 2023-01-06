7f5a3c651c256e5283629e509dae8f7d78a1545d
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2023-01-06 19:37:33 +0800

    working with functionality

--- js/app.js
+++ js/app.js
@@ -1,71 +1,102 @@
 "use strict"
 
 const tipBtn = document.querySelectorAll('#tip__btn');
-const customTipInput = document.querySelector('#custom__tip');
+const customTipInput = document.querySelector('#custom');
 const billAmount = document.querySelector('#bill');
 const numberOfPeople = document.querySelector('#number__of__people');
-const totalAmountPerPerson = document.querySelector('#total__amount__per__person');
+const totalTipPerPerson = document.querySelector('#total__amount__per__person');
 const totalPerPerson = document.querySelector('#total__per__person');
 const resetBtn = document.querySelector('#btn__reset');
+const userInput = document.querySelectorAll('.user__input');
 
+// Check if the window height below 600 
+// then apply min-height with property value of 100vh
+if(window.innerWidth > 900 && window.innerHeight <= 600) {
+    document.querySelector('.main').style.minHeight = '100vh';
+    document.querySelector('.main').style.paddingTop = '3rem';
+}
 
+// Reset all values
 function resetAll () {
-    billAmount.value = '0';
-    numberOfPeople.value = '0';
-    totalAmountPerPerson.textContent = '0.00'; 
+    billAmount.value = '';
+    numberOfPeople.value = '';
+    totalTipPerPerson.textContent = '0.00'; 
     totalPerPerson.textContent = '0.00';
+    customTipInput.value = '';
+    resetBtn.setAttribute('disabled', '');
 }
 
 
+// Tip per person
 function tipPerPersonAmount (input) {
+    if(isNaN(+numberOfPeople.value) || +numberOfPeople.value === 0) return;
+
     const percentage = (+input / 100) * +billAmount.value;
     const total = percentage / +numberOfPeople.value;
+    
+    totalTipPerPerson.textContent = (Math.floor(total * 100) / 100).toFixed(2);
 
-    // TODO 
-    // manipulating the text
-
-    totalPerPersonAmount(total)
+    totalPerPersonAmount(total);
 }
 
+// Total per person
 function totalPerPersonAmount (input) {
+    if(isNaN(+input)) return;
+
     const perPersonAmount = +billAmount.value / +numberOfPeople.value;
-    const totalperson = input + perPersonAmount;
-
-     // TODO 
-    // manipulating the text
-    console.log(totalperson)
+    const totalPerson = +input + perPersonAmount;
+   
+    totalPerPerson.textContent = totalPerson.toFixed(2);
 }
 
-function displayError() {
-    const message = document.querySelector('.error__message');
-    message.classList.remove('hidden');
-    message.style.color = '#ef4444';
-    numberOfPeople.focus();
-    numberOfPeople.style.outlineColor = '#ef4444'
-}
-
-function displaySuccess() {
-    const message = document.querySelector('.error__message');
-    message.classList.add('hidden');
-}
-
-
-function inputSlice (input) {
-    const percentage = input.slice(0, -1);
-
-    if(+numberOfPeople.value === 0) {
-        displayError()
+// Display error/success
+function displayMessage(message) {
+    const span = document.querySelector('.error__message');
+    if(message === 'error') {
+        span.classList.remove('hidden');
+        span.style.color = 'var(--error)';
+        numberOfPeople.focus();
+        numberOfPeople.style.outlineColor = 'var(--error)';
     } else {
-        tipPerPersonAmount(percentage)
-        displaySuccess()
+        span.classList.add('hidden');
+        numberOfPeople.blur();
+        numberOfPeople.style.outlineColor = 'var(--color-001)';
     }
 }
 
+// Selected Tip
+function selectedTipAmount (input) {
+
+    // removing percentage 
+    const percentage = input.slice(0, -1);
+
+    // check if the input(number of people) is empty
+    if(+numberOfPeople.value === 0) {
+        displayMessage('error');
+    } else {
+        tipPerPersonAmount(percentage)
+        displayMessage('success');
+    }
+}
+
+// Loop every tip buttons and add click handler except custom button
 for(let i = 0; i < tipBtn.length; i++) {
     tipBtn[i].addEventListener('click', () => {
-       const percent = tipBtn[i].querySelector('label').textContent;
-        inputSlice(percent);
+        const percent = tipBtn[i].querySelector('label').textContent;
+        selectedTipAmount(percent);
     })
 }
 
+// Loop every inputs
+for(let i = 0; i < userInput.length; i++) {
+    userInput[i].addEventListener('change', (e) => {
+        resetBtn.removeAttribute('disabled');
+        tipPerPersonAmount(e.target.value)
+    })
+}
+
+// reset button
 resetBtn.addEventListener('click', resetAll);
+
+
+
